a78807a69e3c4830fa138b7890eed7b624e5f564
sketch254 <koinbarclay@gmail.com>
2026-05-11 12:16:48 +0300

    brute force solution

--- python/binary_search/binary_search.py
+++ python/binary_search/binary_search.py
@@ -1,4 +1,17 @@
 def search(nums, target):
-    pass
+    left, right = 0, len(nums) - 1
 
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] < target:
+            left = mid + 1
+        elif nums[mid] > target:
+            right = mid - 1
+        else:
+            return mid
+
+    return -1
+
+print(search([1,2,3,4,5,6,7,8,9,0], 7))
     
