0c76db00d2f3eea4f0f535bc20a85302434ef048
sketch254 <koinbarclay@gmail.com>
2026-05-28 18:10:27 +0300

    solution fix

--- python/binary_search/find_index.py
+++ python/binary_search/find_index.py
@@ -1,5 +1,30 @@
-def getIndex(iter):
-    pass
+class ListReader:
+    def __init__(self, arr):
+        self.arr = arr
+
+    def length(self):
+        return len(self.arr)
+
+    def compareSub(self, l, r, x, y):
+        sum1 = sum(self.arr[l:r+1])
+        sum2 = sum(self.arr[x:y+1])
+        if sum1 < sum2: return -1
+        if sum1 > sum2: return 1
+        return 0
 
 
-print(iter)
+def getIndex(reader):
+    left = 0
+    length = reader.length()
+    while length > 1:
+        length //= 2
+        cmp = reader.compareSub(left, left + length - 1, left + length, left + length + length - 1)
+        if cmp == 0:
+            return left + length + length
+        if cmp < 0:
+            left += length
+    return left
+
+
+reader = ListReader([1, 1, 1, 1, 1, 1, 1, 2])
+print(getIndex(reader))  # Expected: 7
