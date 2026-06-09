90769d05e2320e50c9894ebb49d87d069dc8c72a
sketch254 <koinbarclay@gmail.com>
2026-06-09 11:58:08 +0300

    worked on distribution of resources

--- python/binary_search/max_allocation_to_target.py
+++ python/binary_search/max_allocation_to_target.py
@@ -1,9 +1,26 @@
 def maxResourceAllocation(candies, target):
-    n = len(candies)
 
     if sum(candies) < target:
         return 0
 
+    def can_allocate(num):
+        x = sum(piles// num for piles in candies) 
+        return x >= target
+
+    low, high = 0, max(candies)
+    res = 0
+
+    while low < high:
+        mid = (low + high) // 2
+        if can_allocate(mid):
+            res = mid
+            low = mid + 1
+
+        else:
+            high = mid - 1
+
+    return res
+
     
 
-print(maxResourceAllocation([5, 8, 6], 3))
+print(maxResourceAllocation([6, 6, 6], 3))
